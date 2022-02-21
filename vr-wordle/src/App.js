009b78c41bde6f8eb7b12da8e29cfa894db6354d
tomw13 <tomwaghorn13@gmail.com>
2022-02-21 14:52:04 +0000

    installed packages and imported skybox images

--- vr-wordle/src/App.js
+++ vr-wordle/src/App.js
@@ -1,6 +1,9 @@
 import React, { useRef, useState } from "react";
-import { useFrame } from "@react-three/fiber";
+import { useFrame, useThree } from "@react-three/fiber";
+import { CubeTextureLoader } from "three";
 import { VRCanvas } from "@react-three/xr";
+import { OrbitControls, Stars } from "@react-three/drei";
+import forest from "./skyboxes/forest.png";
 
 function Box(props) {
   // This reference gives us direct access to the THREE.Mesh object
@@ -27,17 +30,46 @@ function Box(props) {
   );
 }
 
+function Plane() {
+  return (
+    <mesh position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+      <planeBufferGeometry attach="geometry" args={[100, 100]} />
+      <meshLambertMaterial attach="material" color="lightblue" />
+    </mesh>
+  );
+}
+
+function SkyBox() {
+  const { scene } = useThree();
+  const loader = new CubeTextureLoader();
+
+  const texture = loader.load([
+    "https://6izyu.csb.app/4b.jpg",
+    "https://6izyu.csb.app/3.jpg",
+    "https://6izyu.csb.app/4b.jpg",
+    "https://6izyu.csb.app/4.jpg",
+    "https://6izyu.csb.app/5.jpg",
+    "https://6izyu.csb.app/2.jpg",
+  ]);
+  scene.add(texture);
+  return null;
+}
+
 export default function App() {
   return (
     <VRCanvas>
       <ambientLight intensity={0.5} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <pointLight position={[-10, -10, -10]} />
-      <Box position={[-2.2, 0, 0]} />
-      <Box position={[-0.8, 0, 0]} />
-      <Box position={[0.6, 0, 0]} />
-      <Box position={[2, 0, 0]} />
-      <Box position={[3.4, 0, 0]} />
+      <Box position={[-2.2, 1, 0]} />
+      <Box position={[-0.8, 1, 0]} />
+      <Box position={[0.6, 1, 0]} />
+      <Box position={[2, 1, 0]} />
+      <Box position={[3.4, 1, 0]} />
+      <OrbitControls />
+      <Plane />
+      <Stars />
+      <SkyBox />
     </VRCanvas>
   );
 }
