d1e55e3b08d85ef68e76436ed79745fe1c3a16fa
tomw13 <tomwaghorn13@gmail.com>
2022-02-21 12:11:41 +0000

    added a row of boxes

--- vr-wordle/src/App.js
+++ vr-wordle/src/App.js
@@ -32,8 +32,11 @@ export default function App() {
       <ambientLight intensity={0.5} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <pointLight position={[-10, -10, -10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
+      <Box position={[-2.2, 0, 0]} />
+      <Box position={[-0.8, 0, 0]} />
+      <Box position={[0.6, 0, 0]} />
+      <Box position={[2, 0, 0]} />
+      <Box position={[3.4, 0, 0]} />
     </VRCanvas>
   );
 }
