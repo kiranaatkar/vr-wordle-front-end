097f0e64f41699b548152ba0797c5ff3b4784009
tomw13 <tomwaghorn13@gmail.com>
2022-02-22 16:07:51 +0000

    removed unused variables

--- vr-wordle/src/Components/Grid.js
+++ vr-wordle/src/Components/Grid.js
@@ -1,4 +1,3 @@
-import React, { useState, useRef } from "react";
 import { useThree } from "@react-three/fiber";
 import {
   MeshLambertMaterial,
@@ -7,12 +6,12 @@ import {
   InstancedMesh,
 } from "three";
 
-var mesh;
+let mesh;
 const tempBoxes = new Object3D();
 
 function AddInstancedMesh() {
   console.log("meshed");
-  // An InstancedMesh of 4 cubes
+
   mesh = new InstancedMesh(
     new BoxBufferGeometry(1, 1, 1),
     new MeshLambertMaterial({ color: "white" }),
@@ -25,8 +24,7 @@ function AddInstancedMesh() {
 
 function setInstancedMeshPositions(mesh) {
   console.log("positioned");
-  for (var i = 0; i < mesh.count; i++) {
-    // we add 200 units of distance (the width of the section) between each.
+  for (let i = 0; i < mesh.count; i++) {
     tempBoxes.position.set(
       -2.8 + (i % 5) * 1.4,
       12 - Math.floor(i / 5) * 2,
