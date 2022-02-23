7b15895747ed46b7399a9928107e77fa8bab0b7f
Kiran Aatkar <kiranaatkar@gmail.com>
2022-02-23 11:07:00 +0000

    edit cylinder geometry

--- vr-wordle/src/Components/Cylinder.js
+++ vr-wordle/src/Components/Cylinder.js
@@ -4,8 +4,13 @@ import { useCylinder } from "@react-three/cannon";
 export default function Cylinder(props) {
   // Make the cup a physics object with a big mass
   // args: topRad, bottomRad, height, radialSegments, heightSegments, openEnded
-  const args = [0.5, 0.3, 1, 32, 1, true];
-  const [ref] = useCylinder(() => ({ mass: 10000, args, ...props }));
+  const args = [0.6, 0.4, 0.75, 32, 1, true];
+  const [ref] = useCylinder(() => ({
+    mass: 100000,
+    args,
+    ...props,
+    material: { friction: 0 },
+  }));
 
   return (
     <mesh ref={ref}>
