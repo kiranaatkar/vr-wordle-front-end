cf976ba78781d19dec70c4f77e7564ec4311b147
Kiran Aatkar <kiranaatkar@gmail.com>
2022-02-23 11:24:40 +0000

    added changes as per Tom in VR suggestions

--- vr-wordle/src/Components/Table.js
+++ vr-wordle/src/Components/Table.js
@@ -5,10 +5,10 @@ import { usePlane } from "@react-three/cannon";
 export default function Table(props) {
   const [ref] = usePlane(() => ({
     rotation: [-Math.PI / 2 + 0.3, 0, 0],
-    position: [0, 0.6, -0.8],
+    position: [0, 1.1, -0.8],
     type: "Static",
     material: {
-      friction: 0,
+      friction: 1,
     },
   }));
 
