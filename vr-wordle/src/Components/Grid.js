3f0b27a160eb6d764b4026d6f11aa96731afdb30
tomw13 <tomwaghorn13@gmail.com>
2022-02-25 11:21:01 +0000

    Added rotation animation

--- vr-wordle/src/Components/Grid.js
+++ vr-wordle/src/Components/Grid.js
@@ -1,7 +1,8 @@
 import { Box, Text } from "@react-three/drei";
 import { useBox } from "@react-three/cannon";
 import { useSpring, animated } from "@react-spring/three";
-import { useState } from "react";
+import { useState, useRef } from "react";
+import { useFrame } from "@react-three/fiber";
 
 export default function Grid(props) {
   return props.guesses.map((it, i) => {
@@ -23,46 +24,28 @@ function StaticLetter(props) {
 
   const [active, setActive] = useState(false);
 
-  const { rotation, guessColor, scale } = useSpring({
-    from: { rotation: [Math.PI / 2, 0, 0], guessColor: "#3a3a3c", scale: 1 },
-    to: { rotation: [Math.PI, 0, 0], guessColor: color, scale: 1.5 },
-    config: { mass: 10, tension: 1000, friction: 300, precision: 0.00001 },
+  const box = useRef();
+
+  useFrame(() => {
+    if (props.submitted && box.current.rotation.x < Math.PI / 2) {
+      box.current.rotation.x += 0.02;
+    }
   });
 
-  // const { scale } = useSpring({
-  //   scale: active ? 1.5 : 1,
-  // });
-
-  // const { guessColor } = useSpring({
-  //   guessColor: active ? "red" : "#3a3a3c",
-  // });
-
-  const [box] = useBox(() => ({
-    rotation: [Math.PI / 2, 0, 0],
-    position: props.position,
-    mass: 100,
-    args: size,
-    type: "Static",
-  }));
-
   return (
-    <animated.mesh
+    <Box
       ref={box}
       args={size}
+      rotation={[0, 0, 0]}
       position={props.position}
-      rotation={active ? rotation : [Math.PI / 2, 0, 0]}
-      scale={active ? scale : 1}
+      scale={1}
       onClick={() => {
         console.log(active);
         console.log("clicked");
         setActive(!active);
       }}
     >
-      <boxGeometry attach="geometry"></boxGeometry>
-      <animated.meshStandardMaterial
-        attach="material"
-        color={active ? guessColor : "#3a3a3c"}
-      />
+      <animated.meshStandardMaterial attach="material" color={"#3a3a3c"} />
       <Text
         userData={{ letter: props.id }}
         position={[0, size[1] / 2 + 0.001, 0]}
@@ -72,7 +55,7 @@ function StaticLetter(props) {
       >
         {props.id.toUpperCase()}
       </Text>
-    </animated.mesh>
+    </Box>
   );
 }
 
@@ -86,6 +69,7 @@ function Guess(guess, word) {
         <StaticLetter
           side={side}
           id={it}
+          submitted={it ? true : false}
           key={i}
           position={[-width / 2 + (side + space) * i, 0, 0]}
           state={
