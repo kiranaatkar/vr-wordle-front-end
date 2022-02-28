04a42df7f03e44c8f98cda0bb8429def251362da
Omar-Shueb <omarshueb@Omars-MacBook-Pro.local>
2022-02-28 17:45:30 +0000

    useThree hotfix

--- vr-wordle/src/Components/Wind.js
+++ vr-wordle/src/Components/Wind.js
@@ -1,6 +1,6 @@
 import { useState, useRef, useEffect } from "react";
 import * as THREE from "three";
-import { useThree, useLoader } from "react-three-fiber";
+import { useThree, useLoader } from "@react-three/fiber";
 
 export default function Wind() {
   const wind = "/wind.mp3";
