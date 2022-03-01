a296432c1f4f2fe97b3c09fb06427351c62aa88d
TamoorW <waheedtamoor@gmail.com>
2022-03-01 12:17:36 +0000

    keyboard-not-working

--- vr-wordle/src/Components/Keyboard.js
+++ vr-wordle/src/Components/Keyboard.js
@@ -2,23 +2,67 @@ import React, { useRef, useEffect } from "react";
 import { useFrame, extend } from "@react-three/fiber";
 import ThreeMeshUI from "three-mesh-ui";
 import * as THREE from "three";
+import { useInteraction } from "@react-three/xr";
 import Backspace from "../assets/backspace.png";
 import Shift from "../assets/shift.png";
 import Enter from "../assets/enter.png";
 
 extend(ThreeMeshUI);
 
-export default function Keyboard() {
-  const keys = useRef();
+const colors = {
+  keyboardBack: 0x858585,
+  panelBack: 0x262626,
+  button: 0x363636,
+  hovered: 0x1c1c1c,
+  selected: 0x109c5d,
+};
 
-  useEffect(() => {});
+export default function Keyboard() {
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current.keys.forEach((key) => {
+      key.setupState({
+        state: "idle",
+        attributes: {
+          offset: 0,
+          backgroundColor: new THREE.Color(colors.button),
+          backgroundOpacity: 1,
+        },
+      });
+      key.setupState({
+        state: "hovered",
+        attributes: {
+          offset: 0,
+          backgroundColor: new THREE.Color(colors.hovered),
+          backgroundOpacity: 1,
+        },
+      });
+      key.setupState({
+        state: "selected",
+        attributes: {
+          offset: -0.009,
+          backgroundColor: new THREE.Color(colors.selected),
+          backgroundOpacity: 1,
+        },
+      });
+      key.setState("idle");
+    });
+  });
 
   useFrame(() => {
     ThreeMeshUI.update();
   });
+
+  useInteraction(ref, "onHover", (e) => {
+    // e.intersection.object.setState("hovered");
+    e.intersection.object.parent.parent.setState("hovered");
+    console.log(e.intersection.object.parent);
+  });
+
   return (
     <keyboard
-      ref={keys}
+      ref={ref}
       position={[-3, 1.5, -1]}
       rotation={[0, Math.PI / 4, 0]}
       scale={2}
@@ -28,7 +72,7 @@ export default function Keyboard() {
           fontFamily: "/Roboto-msdf.json",
           fontTexture: "/Roboto-msdf.png",
           fontSize: 0.035, // fontSize will propagate to the keys blocks
-          backgroundColor: new THREE.Color(0x858585),
+          backgroundColor: new THREE.Color(colors.keyboardBack),
           backgroundOpacity: 1,
           backspaceTexture: Backspace,
           shiftTexture: Shift,
