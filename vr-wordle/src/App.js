da06595da8b35c888e633aaaae5001e0e7144ece
tomw13 <tomwaghorn13@gmail.com>
2022-03-02 13:47:38 +0000

    added end game routing to results

--- vr-wordle/src/App.js
+++ vr-wordle/src/App.js
@@ -1,42 +1,56 @@
-import React, { useState, Suspense } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import Game from './Components/Game';
-import Homepage from './Components/Homepage';
-import LoadingScreen from './Components/loadingScreen.js';
-import { Stats } from '@react-three/drei';
-import './App.css';
+import React, { useState, Suspense } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Game from "./Components/Game";
+import Homepage from "./Components/Homepage";
+import LoadingScreen from "./Components/loadingScreen.js";
+import { Stats } from "@react-three/drei";
+import "./App.css";
 
 // FOR TESTING RESULTS COMPONENT
-import Results from './Components/ResultsDisplay';
+import Results from "./Components/ResultsDisplay";
 
 export default function App() {
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState("");
   const [colorBlind, setColorBlind] = useState(false);
+  const [gameEnded, endGame] = useState(false);
 
   console.log(username, colorBlind);
 
   return (
-    <div className='App'>
-      <Results answer='nasty' userScore={3} />
+    <div className="App">
       <Routes>
         <Route
-          path='/play'
+          path="/results"
+          element={
+            username && gameEnded ? (
+              <Results answer="nasty" userScore={3} />
+            ) : (
+              <Navigate to="/" />
+            )
+          }
+        />
+        <Route
+          path="/play"
           element={
             username ? (
               <Suspense fallback={<LoadingScreen />}>
                 <Stats
                   showPanel={0} // Start-up panel (default=0)
-                  className='stats' // Optional className to add to the stats container dom element
+                  className="stats" // Optional className to add to the stats container dom element
                 />
-                <Game username={username} colorBlind={colorBlind} />
+                <Game
+                  username={username}
+                  colorBlind={colorBlind}
+                  endGame={(state) => endGame(state)}
+                />
               </Suspense>
             ) : (
-              <Navigate replace to='/' />
+              <Navigate replace to="/" />
             )
           }
         />
         <Route
-          path='/'
+          path="/"
           element={
             !username ? (
               <Homepage
@@ -44,7 +58,7 @@ export default function App() {
                 setColorBlind={setColorBlind}
               />
             ) : (
-              <Navigate replace to='/play' />
+              <Navigate replace to="/play" />
             )
           }
         />
