212090da209bb7b16acdbef455241383e0858c54
Kiran Aatkar <kiranaatkar@gmail.com>
2022-03-02 11:23:49 +0000

    added a low-poly version of the columns to improve performance

--- vr-wordle/src/Components/Column.js
+++ vr-wordle/src/Components/Column.js
@@ -1,37 +1,36 @@
-import React, { useRef } from "react";
-import { useLoader } from "@react-three/fiber";
-import { TextureLoader } from "three";
-import { useGLTF } from "@react-three/drei";
-import ColumnContainer from "./ColumnContainer.js";
+import React, { useRef } from 'react';
+import { useLoader } from '@react-three/fiber';
+import { TextureLoader } from 'three';
+import { useGLTF } from '@react-three/drei';
+import ColumnContainer from './ColumnContainer.js';
 
 export default function Model({ position, guessIndex, setGuess }) {
   const [colorMap, normalMap, roughnessMap, aoMap] = useLoader(TextureLoader, [
-    "/textures/aerial_rocks_02_diff_2k.jpg",
-    "/textures/aerial_rocks_02_nor_dx_2k.jpg",
-    "/textures/aerial_rocks_02_rough_2k.jpg",
-    "/textures/aerial_rocks_02_ao_2k.jpg",
+    '/textures/aerial_rocks_02_diff_2k.jpg',
+    '/textures/aerial_rocks_02_nor_dx_2k.jpg',
+    '/textures/aerial_rocks_02_rough_2k.jpg',
+    '/textures/aerial_rocks_02_ao_2k.jpg',
   ]);
 
   const group = useRef();
-  const { nodes } = useGLTF("/scene.glb");
+  const { nodes } = useGLTF('/lowPolyColumn.glb');
+
   return (
     <>
       <mesh
-        rotation={[-Math.PI / 2, 0, 0]}
         ref={group}
         position={position}
         dispose={null}
         scale={1.5}
         castShadow
         receiveShadow
-        geometry={nodes.Object_2.geometry}
-      >
+        geometry={nodes.mesh_0.geometry}>
         <meshStandardMaterial
           map={colorMap}
           normalMap={normalMap}
           roughnessMap={roughnessMap}
           aoMap={aoMap}
-        />{" "}
+        />{' '}
       </mesh>
       <ColumnContainer
         columnGeometry={[...position, 0.3]}
@@ -42,4 +41,4 @@ export default function Model({ position, guessIndex, setGuess }) {
   );
 }
 
-useGLTF.preload("/scene.glb");
+useGLTF.preload('/scene.glb');
--- vr-wordle/src/Components/Pillars.js
+++ vr-wordle/src/Components/Pillars.js
@@ -1,30 +1,30 @@
-import Column from "./Column.js";
+import Column from './Column.js';
 
 export default function Pillars(props) {
   return (
     <>
       <Column
-        position={[-1.25, 0, 0.4]}
+        position={[-1.25, 0.5, 0.4]}
         guessIndex={4}
         setGuess={props.setGuess}
       />
       <Column
-        position={[-0.6, 0, 0.6]}
+        position={[-0.6, 0.5, 0.6]}
         guessIndex={3}
         setGuess={props.setGuess}
       />
       <Column
-        position={[0, -0.1, 0.7]}
+        position={[0, 0.5, 0.7]}
         guessIndex={2}
         setGuess={props.setGuess}
       />
       <Column
-        position={[0.6, 0, 0.6]}
+        position={[0.6, 0.5, 0.6]}
         guessIndex={1}
         setGuess={props.setGuess}
       />
       <Column
-        position={[1.2, 0, 0.4]}
+        position={[1.2, 0.5, 0.4]}
         guessIndex={0}
         setGuess={props.setGuess}
       />
