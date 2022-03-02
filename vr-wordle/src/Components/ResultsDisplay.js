5caa76213fcdebabc7e27847d1f0d25ca6c2b020
tomw13 <tomwaghorn13@gmail.com>
2022-03-02 13:59:26 +0000

    fixed deployment issues

--- vr-wordle/src/Components/ResultsDisplay.js
+++ vr-wordle/src/Components/ResultsDisplay.js
@@ -1,15 +1,7 @@
-import { useEffect, useState } from 'react';
-import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  Tooltip,
-  CartesianGrid,
-  Cell,
-} from 'recharts';
-import Networking from './Networking.js';
-import './results.css';
+import { useEffect, useState } from "react";
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Cell } from "recharts";
+import Networking from "./Networking.js";
+import "./results.css";
 
 export default function Results(props) {
   // const {answer} = props
@@ -25,41 +17,42 @@ export default function Results(props) {
     { score: 6, sum: 0 },
   ]);
 
-  useEffect(async () => {
-    const json = await myAPI.getWordScores(answer);
-    json.scores.forEach((entry) => {
-      const scoreIndex = data.findIndex((data) => data.score === entry.score);
-      data[scoreIndex].sum++;
-    });
-    setData(data);
-    StillLoading(false);
-  }, []);
+  // useEffect(async () => {
+  //   const json = await myAPI.getWordScores(answer);
+  //   json.scores.forEach((entry) => {
+  //     const scoreIndex = data.findIndex((data) => data.score === entry.score);
+  //     data[scoreIndex].sum++;
+  //   });
+  //   setData(data);
+  //   StillLoading(false);
+  // }, []);
 
   return (
     <div>
       {loading ? (
-        'Loading...'
+        "Loading..."
       ) : (
-        <div className='graph-wrapper'>
+        <div className="graph-wrapper">
           <BarChart
             width={730}
             height={250}
             data={data}
-            layout='vertical'
-            barCategoryGap={1}>
+            layout="vertical"
+            barCategoryGap={1}
+          >
             <XAxis hide />
             <YAxis
-              dataKey='score'
-              type='number'
-              interval='preserveStartEnd'
+              dataKey="score"
+              type="number"
+              interval="preserveStartEnd"
               domain={[1, 6]}
             />
             <Tooltip />
-            <Bar dataKey='sum' fill='black'>
+            <Bar dataKey="sum" fill="black">
               {data.map((entry) => (
                 <Cell
                   key={entry}
-                  fill={entry.score === userScore ? '#99f2c8' : '#1f4037'}
+                  fill={entry.score === userScore ? "#99f2c8" : "#1f4037"}
                   label={entry.sum}
                 />
               ))}
