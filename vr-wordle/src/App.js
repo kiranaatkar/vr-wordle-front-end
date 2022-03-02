ad56e6b7f684cd1a3875c80d7e73edd86a2ab308
Omar-Shueb <omarshueb@Omars-MacBook-Pro.local>
2022-03-02 09:12:18 +0000

    Added css to be more standard

--- vr-wordle/src/App.js
+++ vr-wordle/src/App.js
@@ -1,39 +1,45 @@
-import React, { useState, Suspense } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import Game from './Components/Game';
-import Homepage from './Components/Homepage';
-import LoadingScreen from './Components/loadingScreen.js';
+import React, { useState, Suspense } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Game from "./Components/Game";
+import Homepage from "./Components/Homepage";
+import LoadingScreen from "./Components/loadingScreen.js";
+import "./App.css";
 
 export default function App() {
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState("");
   const [colorBlind, setColorBlind] = useState(false);
 
   console.log(username, colorBlind);
 
   return (
-    <Routes>
-      <Route
-        path='/play'
-        element={
-          username ? (
-            <Suspense fallback={<LoadingScreen />}>
-              <Game username={username} colorBlind={colorBlind} />
-            </Suspense>
-          ) : (
-            <Navigate replace to='/' />
-          )
-        }
-      />
-      <Route
-        path='/'
-        element={
-          !username ? (
-            <Homepage setUsername={setUsername} setColorBlind={setColorBlind} />
-          ) : (
-            <Navigate replace to='/play' />
-          )
-        }
-      />
-    </Routes>
+    <div className="App">
+      <Routes>
+        <Route
+          path="/play"
+          element={
+            username ? (
+              <Suspense fallback={<LoadingScreen />}>
+                <Game username={username} colorBlind={colorBlind} />
+              </Suspense>
+            ) : (
+              <Navigate replace to="/" />
+            )
+          }
+        />
+        <Route
+          path="/"
+          element={
+            !username ? (
+              <Homepage
+                setUsername={setUsername}
+                setColorBlind={setColorBlind}
+              />
+            ) : (
+              <Navigate replace to="/play" />
+            )
+          }
+        />
+      </Routes>
+    </div>
   );
 }
