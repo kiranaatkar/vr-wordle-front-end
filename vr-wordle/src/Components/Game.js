01a61c4fd8af16b70657f09c9a40a56ecb6f1d98
Omar-Shueb <omarshueb@Omars-MacBook-Pro.local>
2022-03-04 10:54:22 +0000

    Added allowed words for production builds

--- vr-wordle/src/Components/Game.js
+++ vr-wordle/src/Components/Game.js
@@ -1,24 +1,25 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { Environment, Sky, Stars } from '@react-three/drei';
-import { VRCanvas, DefaultXRControllers, Hands } from '@react-three/xr';
-import { differenceInDays, format } from 'date-fns';
-import { Navigate } from 'react-router';
-import { answerWords } from '../word-lists/answer-words.js';
-import { useCookies } from 'react-cookie';
-import { Physics } from '@react-three/cannon';
-import LetterCubes from './LetterCubes.js';
-import GameEnd from './GameEnd.js';
-import Floor from './Floor.js';
-import Button from './Button.js';
-import Submit from './Submit.js';
-import Grid from './Grid.js';
-import Grabber from './Grab.js';
-import Table from './Table.js';
-import Player from './Player.js';
-import Letter from './Letter.js';
-import Pillars from './Pillars.js';
-import Networking from './Networking.js';
-import Alphabet from './Alphabet.js';
+import React, { useState, useRef, useEffect } from "react";
+import { Environment, Sky, Stars } from "@react-three/drei";
+import { VRCanvas, DefaultXRControllers, Hands } from "@react-three/xr";
+import { differenceInDays, format } from "date-fns";
+import { Navigate } from "react-router";
+import { answerWords } from "../word-lists/answer-words.js";
+import { allowedWords } from "../word-lists/allowed-words.js";
+import { useCookies } from "react-cookie";
+import { Physics } from "@react-three/cannon";
+import LetterCubes from "./LetterCubes.js";
+import GameEnd from "./GameEnd.js";
+import Floor from "./Floor.js";
+import Button from "./Button.js";
+import Submit from "./Submit.js";
+import Grid from "./Grid.js";
+import Grabber from "./Grab.js";
+import Table from "./Table.js";
+import Player from "./Player.js";
+import Letter from "./Letter.js";
+import Pillars from "./Pillars.js";
+import Networking from "./Networking.js";
+import Alphabet from "./Alphabet.js";
 
 const myAPI = new Networking();
 
@@ -49,7 +50,7 @@ export function generateLetters(reset, alphabet, letters) {
 
 function getRandomAnswerWord() {
   const dateOne = new Date();
-  const dateTwo = new Date('02/24/2022');
+  const dateTwo = new Date("02/24/2022");
   let answer = answerWords[differenceInDays(dateOne, dateTwo) + 250];
   return answer;
 }
@@ -59,11 +60,11 @@ export default function Game(props) {
   const [username] = useState(props.username);
   const [colorBlind] = useState(props.colorBlind);
   const [guessCount, setGuessCount] = useState(
-    cookies.guesses.filter((guess) => guess !== '     ').length
+    cookies.guesses.filter((guess) => guess !== "     ").length
   );
   const [reset, setReset] = useState(false);
   const [currentGuess, setCurrentGuess] = useState([]);
-  const [answer, setAnswer] = useState('');
+  const [answer, setAnswer] = useState("");
   const [gameEnd, setGameCondition] = useState(false);
   const [playing, setPlaying] = useState(true);
 
@@ -73,30 +74,30 @@ export default function Game(props) {
 
   useEffect(() => {
     setAnswer(
-      process.env.NODE_ENV === 'development' ? 'nnnnn' : getRandomAnswerWord()
+      process.env.NODE_ENV === "development" ? "nnnnn" : getRandomAnswerWord()
     );
-    let todaysDate = format(new Date(), 'yyyy-MM-dd');
+    let todaysDate = format(new Date(), "yyyy-MM-dd");
     if (!cookies.guesses || todaysDate !== cookies.date) {
-      setCookie('guesses', [
-        '     ',
-        '     ',
-        '     ',
-        '     ',
-        '     ',
-        '     ',
+      setCookie("guesses", [
+        "     ",
+        "     ",
+        "     ",
+        "     ",
+        "     ",
+        "     ",
       ]);
-      setCookie('date', format(new Date(), 'yyyy-MM-dd'));
+      setCookie("date", format(new Date(), "yyyy-MM-dd"));
     }
   }, []);
 
   props.setAnswer(answer);
 
-  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
   const letters = useRef(<group />);
 
   const deleteOldGuess = () => {
     for (const letter of currentGuess) {
-      if (!getRandomAnswerWord().split('').includes(letter)) {
+      if (!getRandomAnswerWord().split("").includes(letter)) {
         const indexToRemove = letters.current.children.findIndex((child) => {
           return child.children[0].name === letter;
         });
@@ -116,19 +117,27 @@ export default function Game(props) {
   };
 
   const submitGuess = async () => {
-    if (
-      guessCount < 6 &&
-      currentGuess.filter((char) => char !== '').length === 5 &&
-      playing
-      // [...allowedWords, ...answerWords].includes(currentGuess.join(""))
-    ) {
+    let conditions = null;
+
+    process.env.NODE_ENV === "development" //Allows for non-allowed words during development and testing
+      ? (conditions =
+          guessCount < 6 &&
+          currentGuess.filter((char) => char !== "").length === 5 &&
+          playing)
+      : (conditions =
+          guessCount < 6 &&
+          currentGuess.filter((char) => char !== "").length === 5 &&
+          playing &&
+          [...allowedWords, ...answerWords].includes(currentGuess.join("")));
+
+    if (conditions) {
       const newGuesses = cookies.guesses;
-      newGuesses[guessCount] = currentGuess.join('');
+      newGuesses[guessCount] = currentGuess.join("");
       const newCount = guessCount + 1;
       setGuessCount(newCount);
       deleteOldGuess();
       resetPositions();
-      if (currentGuess.join('') === answer) {
+      if (currentGuess.join("") === answer) {
         setPlaying(false);
         const score = guessCount + 1;
         const endTime = new Date().getSeconds();
@@ -137,40 +146,41 @@ export default function Game(props) {
         await myAPI.postScore(score, answer, username, gameTime);
         props.setScore(guessCount);
         setTimeout(async () => {
-          setGameCondition('win');
+          setGameCondition("win");
         }, 3000);
       } else if (!newGuesses.includes(answer) && guessCount === 5) {
         setPlaying(false);
         props.setScore(null);
         setTimeout(async () => {
-          setGameCondition('lose');
+          setGameCondition("lose");
         }, 3000);
       }
-      setCookie('guesses', newGuesses);
+      setCookie("guesses", newGuesses);
     }
   };
 
   return username ? (
     <VRCanvas
-      mode='concurrent'
+      mode="concurrent"
       performance={{ min: 0.8 }}
-      style={{ touchAction: 'none' }}
-      frameloop='demand'
+      style={{ touchAction: "none" }}
+      frameloop="demand"
       sessionInit={{
         optionalFeatures: [
-          'local-floor',
-          'bounded-floor',
-          'hand-tracking',
-          'layers',
+          "local-floor",
+          "bounded-floor",
+          "hand-tracking",
+          "layers",
         ],
-      }}>
+      }}
+    >
       {/* Renders a component that will end the VR session and redirect to results
       on game end. */}
       {gameEnd && <GameEnd endGame={() => props.endGame(true)} />}
 
       {/* Grabs Oculus Controllers */}
       <DefaultXRControllers />
-      <Hands modelLeft={'/hand-left.gltf'} modelRight={'/hand-right.gltf'} />
+      <Hands modelLeft={"/hand-left.gltf"} modelRight={"/hand-right.gltf"} />
       <ambientLight intensity={0.3} />
       <Grid guesses={cookies.guesses} answer={answer} colorBlind={colorBlind} />
       {/* Adds Physics to child elements */}
@@ -186,15 +196,15 @@ export default function Game(props) {
         {/* Allows grabbing of the individual letters */}
         <Grabber groupRef={letters} />
         {generateLetters(reset, alphabet, letters)}
-        {process.env.NODE_ENV === 'development' && (
-          <Letter position={[2, 1, -1]} name='n' />
+        {process.env.NODE_ENV === "development" && (
+          <Letter position={[2, 1, -1]} name="n" />
         )}
 
         <Pillars setGuess={setGuess} />
         <Player />
         <Floor />
       </Physics>
-      <Environment preset={'night'} />
+      <Environment preset={"night"} />
       <Sky distance={450000} sunPosition={[0, -1, 0]} azimuth={0.25} />
       <Stars
         radius={100}
@@ -204,7 +214,7 @@ export default function Game(props) {
         saturation={0}
         fade
       />
-      <fog attach='fog' args={['#421700', 0, 100]} />
+      <fog attach="fog" args={["#421700", 0, 100]} />
 
       {/* 
       Renders an alphabet behind and underneath the player to cache every 
@@ -212,6 +222,6 @@ export default function Game(props) {
       <Alphabet />
     </VRCanvas>
   ) : (
-    <Navigate to='/' />
+    <Navigate to="/" />
   );
 }
