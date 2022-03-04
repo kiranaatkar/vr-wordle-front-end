b9e4dac24166d9b0a718c7fb73d1e359153061df
tomw13 <tomwaghorn13@gmail.com>
2022-03-04 11:18:27 +0000

    improved testing, split into 3 test suites, tested format time

--- vr-wordle/src/App.test.js
+++ vr-wordle/src/App.test.js
@@ -1,8 +1,5 @@
 import { answerWords } from "./word-lists/answer-words.js";
 import { differenceInDays } from "date-fns";
-import { render, screen } from "@testing-library/react";
-import React, { useState, useRef } from "react";
-import Grid, { Guess } from "./Components/Grid.js";
 
 const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 
@@ -467,67 +464,4 @@ test("submitGuess ends the game on last guess", () => {
   submitGuess();
 
   expect(state.guessCount).toBe(6);
-});
-
-test("Grid displays 30 blocks", () => {
-  const guesses = ["irate", "choke", "chore", "chose", "crane", "rupee"];
-  const answer = "rupee";
-
-  const test = Grid({ guesses: guesses, answer: answer });
-
-  for (let child in test) {
-    expect(test[child].props.children.props.children.length).toBe(5);
-  }
-});
-
-test("Guess correctly outputs a five letter word with five blocks", () => {
-  const expected = [
-    {
-      side: 1,
-      id: "r",
-      submitted: true,
-      index: 0,
-      position: [-2.2, 0, 0],
-      state: "present",
-    },
-    {
-      side: 1,
-      id: "u",
-      submitted: true,
-      index: 1,
-      position: [-1.1, 0, 0],
-      state: "absent",
-    },
-    {
-      side: 1,
-      id: "p",
-      submitted: true,
-      index: 2,
-      position: [0, 0, 0],
-      state: "absent",
-    },
-    {
-      side: 1,
-      id: "e",
-      submitted: true,
-      index: 3,
-      position: [1.1, 0, 0],
-      state: "present",
-    },
-    {
-      side: 1,
-      id: "e",
-      submitted: true,
-      index: 4,
-      position: [2.2, 0, 0],
-      state: "correct",
-    },
-  ];
-
-  const test = Guess("rupee", "irate");
-  for (let child in test.props.children) {
-    expect(JSON.stringify(test.props.children[child].props)).toStrictEqual(
-      JSON.stringify(expected[child])
-    );
-  }
 });
