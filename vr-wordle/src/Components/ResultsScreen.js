92013f8eecd5bba49fbb45870158f928b5264677
tomw13 <tomwaghorn13@gmail.com>
2022-03-04 10:33:13 +0000

    formatted timings better

--- vr-wordle/src/Components/ResultsScreen.js
+++ vr-wordle/src/Components/ResultsScreen.js
@@ -1,17 +1,33 @@
-import ResultsGraph from './ResultsGraph';
-import './results.css';
+import ResultsGraph from "./ResultsGraph";
+import { intervalToDuration } from "date-fns";
+import "./results.css";
 
 export default function ResultsScreen(props) {
   const { answer, userScore, gameTime } = props;
+  let userTime = intervalToDuration({ start: 0, end: gameTime });
+  let formattedTime = ``;
+  if (userTime.minutes) {
+    formattedTime +=
+      userTime.minutes === 1 ? `1 minute` : `${userTime.minutes} minutes`;
+  }
+  if (userTime.seconds && userTime.minutes) {
+    formattedTime +=
+      userTime.seconds === 1
+        ? " and 1 second"
+        : ` and ${userTime.seconds} seconds`;
+  } else if (userTime.seconds) {
+    formattedTime +=
+      userTime.seconds === 1 ? "1 second" : `${userTime.seconds} seconds`;
+  }
   return (
-    <div className='results-page-wrapper'>
-      <h1 className='graph-text'>Statistics</h1>
-      <h3 className='graph-text'>
+    <div className="results-page-wrapper">
+      <h1 className="graph-text">Statistics</h1>
+      <h3 className="graph-text">
         {userScore
-          ? `You got it in ${userScore} ${
-              userScore > 1 ? 'attempts' : 'attempt'
-            } (${gameTime}s)!`
-          : 'Better luck tomorrow!'}
+          ? `You got it with ${userScore} ${
+              userScore > 1 ? "attempts" : "attempt"
+            } in ${formattedTime}!`
+          : "Better luck tomorrow!"}
       </h3>
       <ResultsGraph answer={answer} userScore={userScore} />
     </div>
